7501d95c3a2d1e93d945871447f79dcddbdfc110
gilles <gilles.maurer@utbm.fr>
2023-03-15 09:14:35 +0100

    change navbar

--- navbar.php
+++ navbar.php
@@ -1,5 +1,8 @@
+<!-- Bandeau sans log -->
 <div class="navbar">
     <a href="./index.php">Accueil</a>
-    <a href="#">Page 2</a>
-    <a href="#">Page 3</a>
+    <a href="./login.php">Log In</a>
+    <a href="./signin.php">Sign In</a>
+    <a href="./classement.php">Classement</a>
+    <a href="./blog.php">Blog</a>
 </div>
