84541720cf800f04cbba876cd372818985cf23d1
gilles-maurer <104887265+gilles-maurer@users.noreply.github.com>
2023-03-15 09:39:45 +0100

    Delete navbar.php

--- navbar.php
+++
@@ -1,8 +0,0 @@
-<!-- Bandeau sans log -->
-<div class="navbar">
-    <a href="./index.php">Accueil</a>
-    <a href="./login.php">Log In</a>
-    <a href="./signin.php">Sign In</a>
-    <a href="./classement.php">Classement</a>
-    <a href="./blog.php">Blog</a>
-</div>
