fa87ef905336b012b182ab47cee2151d66d404b2
gilles <gilles.maurer@utbm.fr>
2023-03-16 15:39:41 +0100

    ajout début page profil

--- SousPages/sqlfunctions.php
+++ SousPages/sqlfunctions.php
@@ -141,6 +141,16 @@
     }
 
 
+    // fonction page profil 
+
+    function get_informations($connexion, $id) {
+
+        $sql = "SELECT * FROM utilisateur WHERE id_utilisateur = '".$id."'";
+        $result = $connexion->query($sql);
+
+        return $result;
+    }
+
 
 
 ?>
--- stats.php
+++ stats.php
@@ -11,6 +11,61 @@
 
 <?php include("SousPages/navbar.php");?>
 
+<?php
+    if(isset($_POST['id_utilisateur'])) {
+        // delete cookie
+        setcookie('id_utilisateur');
+        header('Location: ./index.php');
+    }
+
+?>
+    <h2> Mon profil </h2>
+
+    <?php
+    require("SousPages/connectionbdd.php");
+    $connection = connect_db(); 
+
+    require ("SousPages/sqlfunctions.php");
+
+    $id = $_COOKIE['id_utilisateur'];
+
+    $result = get_informations($connection, $id);
+
+    $row = $result->fetch();
+
+    $date = new DateTime($row['date_naissance']);
+    $date_actuelle = new DateTime();
+    $age = $date->diff($date_actuelle);
+
+    // affiche les informations de l'utilisateur
+
+    echo "<p> Nom : ".$row['nom']."</p>";
+    echo "<p> Prénom : ".$row['prenom']."</p>";
+    echo "<p> Email : ".$row['email']."</p>";
+    echo "<p> Date de naissance : ".$row['date_naissance']."</p>";
+    echo "<p> Age : ".$age->y."</p>";
+    echo "<p> Date d'inscription : ".$row['date_inscription']."</p>";
+
+
+
+
+    ?>
+
+    <!-- T'en fait ce que tu veux de ce form mdr  -->
+    <div>
+
+        <form action='#' method='post'>
+            
+            <div>
+                <!-- Caché -->
+                <input type="hidden" name="id_utilisateur" value="<?php echo $id; ?>">
+            </div>
+            <div>
+                <button type="submit">Se déconnecter</button>
+            </div>
+        </form>
+
+    </div>
 
 </body>
 
