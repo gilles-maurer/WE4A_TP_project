2db2f216101cddef6dc9ba1d27e499675de64d0f
Buckeeeeet <clement.brichet701@gmail.com>
2023-04-12 08:43:14 +0200

    Ajout de commentaires

--- stats.php
+++ stats.php
@@ -12,9 +12,9 @@
 <?php include("SousPages/navbar.php");?>
 <div id="MainContainer">
     <?php
-        if(isset($_POST['id_utilisateur'])) {
-            // delete cookie
-            setcookie('id_utilisateur');
+        //Pour la déconnexion : setcookie sans durée
+        if(isset($_POST['deconnexion'])) {
+            setcookie('id_utilisateur'); // delete cookie
             header('Location: ./index.php');
         }
 
@@ -40,69 +40,79 @@
         $date_actuelle = new DateTime();
         $age = $date->diff($date_actuelle);
 
-        // affiche les informations de l'utilisateur
 
-        echo "<p> Nom : ".$row['nom']."</p>";
-        echo "<p> Prénom : ".$row['prenom']."</p>";
-        echo "<p> Email : ".$row['email']."</p>";
-        echo "<p> Date de naissance : ".$row['date_naissance']."</p>";
-        echo "<p> Age : ".$age->y."</p>";
-        echo "<p> Date d'inscription : ".$row['date_inscription']."</p>";
 
+        /* =============================================================
+        =================== Informations Utilisateur ===================
+        ============================================================= */
+        echo "<p> Nom : ".$row['nom']."</p>
+            <p> Prénom : ".$row['prenom']."</p>
+            <p> Email : ".$row['email']."</p>
+            <p> Date de naissance : ".$row['date_naissance']."</p>
+            <p> Age : ".$age->y."</p>
+            <p> Date d'inscription : ".$row['date_inscription']."</p>";
+
+
+        /* =============================================================
+        ========================== Abonnements =========================
+        ============================================================= */
         echo "<hr>";
         echo "<h2> Mes abonnements </h2>";
 
         $result = get_abonnement($connection, $id);
 
-        echo "<div class='box-invisible'>";
 
         while($row = $result->fetch()) {
             $id_suivie = $row['ID_suivie'];
             $result2 = get_informations($connection, $id_suivie);
             $row2 = $result2->fetch();
 
-            echo "<form action='blog.php'>
-                    <input type='hidden' name='blog' value='".$row['ID_suivie']."'>
-                    <button type='submit'>".$row2['nom']." ".$row2['prenom']."</button>
-                </form>";  
-        
+            echo "<div class='box-invisible'>
+                    <form action='blog.php'>
+                        <input type='hidden' name='blog' value='".$row['ID_suivie']."'>
+                        <button type='submit'>".$row2['nom']." ".$row2['prenom']."</button>
+                    </form>
+                </div>";  
+               //On ajoute un div à chaque itération pour être sûr que chaque bouton est séparé du précédent
         }
 
-        echo "</div>";
 
+
+
+        /* =============================================================
+        ============================ Abonnés ===========================
+        ============================================================= */
         echo "<hr>";
         echo "<h2> Mes abonnés </h2>";
 
         $result = get_abonne($connection, $id);
 
-        echo "<div class='box-invisible'>";
+        echo "";
 
         while($row = $result->fetch()) {
             $id_suiveur = $row['ID_suiveur'];
             $result2 = get_informations($connection, $id_suiveur);
             $row2 = $result2->fetch();
 
-            echo "<form action='blog.php'>
-                    <input type='hidden' name='blog' value='".$row['ID_suiveur']."'>
-                    <button type='submit'>".$row2['nom']." ".$row2['prenom']."</button>
-                </form>";          
+            echo "<div class='box-invisible'>
+                    <form action='blog.php'>
+                        <input type='hidden' name='blog' value='".$row['ID_suiveur']."'>
+                        <button type='submit'>".$row2['nom']." ".$row2['prenom']."</button>
+                    </form><br>
+                </div>";          
             
             }
-
-        echo "</div>";
-
-
         ?>
 
-        <!-- bouton déconnexion -->
 
+
+        <!-- Bouton déconnexion -->
         <hr>
-
         <div>
             <form action='#' method='post'>
                 
                 <div>
-                    <input type="hidden" name="id_utilisateur" value="<?php echo $id; ?>">
+                    <input type="hidden" name="deconnexion" value="<?php echo $id; ?>">
                 </div>
                 <div>
                     <button type="submit">Se déconnecter</button>
