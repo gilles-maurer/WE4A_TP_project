3e3fd3f476e167eb201fc66bf60bf1f8ddca8481
Buckeeeeet <clement.brichet701@gmail.com>
2023-04-12 09:13:21 +0200

    Amélioration de la lisibilité du code

--- blog.php
+++ blog.php
@@ -7,33 +7,6 @@
 <link rel="stylesheet" href="./Styles/style.css"> 
 </head>
 
-<!-- <script> 
-
-function notification($id_post) {
-
-    if (confirm("Voulez vous vraiment supprimer ce post ?")) { 
-
-        window.location.href = "deletepost.php?post="+$id_post;
-
-    }
-}
-
-</script> -->
-
-<?php
-
-    // if(isset($_POST["delete"])) {
-        
-        ?>
-        <script>
-    //         notification();
-        </script>
-        <?php
-    // }
-
-?> 
-
-
 <body>
 
 <?php include("SousPages/navbar.php");?>
@@ -56,25 +29,23 @@ function notification($id_post) {
         <?php 
 
             /*  
-            
-                soit on arrive depuis le bandeau -> on a un cookie si on est connecté
-                                                -> on a pas de cookie, pas de get et pas de post
+            Soit on arrive depuis le bandeau :
+                -> Si on est connecté, on a un Cookie
+                -> Sinon, on a pas de Cookie, pas de Get et pas de Post
 
-                soit on arrive depuis le login -> on a un cookie
-                                                
-                soit on arrive depuis un lien -> on a un get 
-
-                soit on arrive depuis un bouton -> on a un get 
-
-                soit on revient depuis le recherche -> on a un get
-
+            Soit on arrive depuis le login : on a un Cookie
+                                            
+            Soit on arrive depuis un lien, un bouton ou la recherche : on a un Get 
             */
 
-            if (isset($_GET["blog"])) { // si on a un get tout va bien
+            if (isset($_GET["blog"])) {
 
+                // Si on a un Get (-> via tout sauf bandeau) tout va bien
                 $blog = $_GET["blog"];
 
-            } else if (isset($_COOKIE["id_utilisateur"])) { // si pas de get mais qu'on a un cookie, on le prend et on relance la page
+            } else if (isset($_COOKIE["id_utilisateur"])) { 
+
+                // Si pas de Get mais qu'on a un Cookie (-> via le bandeau), on le prend et on relance la page
                 $blog = $_COOKIE["id_utilisateur"];
 
                 ?> 
@@ -88,7 +59,9 @@ function notification($id_post) {
 
                 <?php
 
-            } else if (isset($_POST["blog"])) { // en théorie, le post n'est jamais set mais par sécurité on le rajoute
+            } else if (isset($_POST["blog"])) { 
+                
+                // En théorie, le post n'est jamais set mais par sécurité on le rajoute
                 $blog = $_POST["blog"];
 
                 ?> 
@@ -103,10 +76,14 @@ function notification($id_post) {
                 <?php
 
             } else {
+
+                // Si on a rien reçu, alors l'utilisateur n'est pas connecté et n'a pas accédé à un blog
                 $blog = 0;
             }
 
             
+
+
             if (isset($_GET["nb_post"])) {
                 $nb_post = $_GET["nb_post"];
             } else {
@@ -119,47 +96,53 @@ function notification($id_post) {
                 $recherche = "";
             }
 
-        ?> 
-        <?php
+
 
 
             if ($blog == 0) {
-                echo "<p>Tu n'es sur aucun blog. Connecte toi pour en avoir un, ou recherche celui de quelqu'un d'autre !<p>";
+
+                echo "<p>Tu n'es sur aucun blog. Connecte toi pour en avoir un, ou recherche celui de quelqu'un d'autre.<p>";
+
             } else {
             
-
                 $titre = select_titre_blog($connexion, $blog);
 
                 $titre = $titre->fetch();
 
-                echo "<h1> Blog de ".$titre['prenom']." ".$titre['nom']."</h1>";
+                echo "<h1> Blog de ".$titre['prenom']." ".$titre['nom']."</h1><hr>";
 
-                echo "<hr>"; 
-
+                // Si l'utilisateur est connecté :
                 if (isset($_COOKIE["id_utilisateur"])) {
+
+                    //Si l'utilisateur est sur son propre blog :
                     if ($_COOKIE["id_utilisateur"] == $blog) {
+
                         echo "<form method='post' action='post.php'>
                             <input type='submit' value='Ajouter un post'>
                         </form>";
+
+                        // S'il est sur le blog de quelqu'un d'autre :
                     } else {
 
                         $result = get_nb_abonnement($connexion, $blog, $_COOKIE["id_utilisateur"]);
                         $result = $result->fetch();
 
+                        // Si l'utilisateur n'est pas ou est abonné (le dernier input change en fonction)
                         if ($result["nb_abonnement"] == 0) {
                             echo "<form method='post' action='SousPages/abonnement.php'>
-                                <input type='hidden' name='id_suivie' value='".$blog."'>
-                                <input type='hidden' name='id_suiveur' value='".$_COOKIE["id_utilisateur"]."'>
-                                <input type='submit' value='Suivre'>
+                                    <input type='hidden' name='id_suivie' value='".$blog."'>
+                                    <input type='hidden' name='id_suiveur' value='".$_COOKIE["id_utilisateur"]."'>
+                                    <input type='submit' value='Suivre'>
                                 </form>";
                         } else {
                             echo "<form method='post' action='SousPages/abonnement.php'>
-                                <input type='hidden' name='id_suivie' value='".$blog."'>
-                                <input type='hidden' name='id_suiveur' value='".$_COOKIE["id_utilisateur"]."'>
-                                <input type='submit' value='Ne plus suivre'>
+                                    <input type='hidden' name='id_suivie' value='".$blog."'>
+                                    <input type='hidden' name='id_suiveur' value='".$_COOKIE["id_utilisateur"]."'>
+                                    <input type='submit' value='Ne plus suivre'>
                                 </form>";
                         }
 
+                        // On récupère les stats totales de la personne dont on visite le blog
                         $result = get_stats_blog($connexion, $blog); 
 
                         $result = $result->fetch(); 
@@ -171,9 +154,9 @@ function notification($id_post) {
                         echo "<p>Distance parcourue : ".$result["distance"]." km</p>";
                         
                         if ($temps_heure == 0) {
-                            echo "<p>A couru pendant ".$temps_minute." min !</p>";
+                            echo "<p>A couru pendant : ".$temps_minute." min</p>";
                         } else {
-                            echo "<p>A couru pendant ".$temps_heure." h et ".$temps_minute." min !</p>";
+                            echo "<p>A couru pendant : ".$temps_heure." h et ".$temps_minute." min</p>";
                         }
 
                         echo "<p>Nombre de courses : ".$result["nb_courses"]."</p>";
@@ -187,22 +170,20 @@ function notification($id_post) {
 
                 echo "<hr>";
 
-
-
+                // On vérifie le nombre de posts à afficher
                 if ($nb_post == "no_limit") {
                     $result = select_blog($connexion, $blog);
                 } else {
                     $result = select_blog_limited($connexion, $blog, $nb_post);
                 }
 
-                // nom, prenom, date, distance, temps, vitesse, commentaire, lieu, nb like
-
+                // Ordre d'affichage : nom, prenom, date, distance, temps, vitesse, commentaire, lieu, nb like
                 while ($row = $result->fetch()) {
                     
                     $count_like = count_like($connexion, $row['id_post']);
                     $count_like = $count_like->fetch();
 
-                    echo "<div class='left'>
+                    echo "
 
                     <p>".$row['date']."</p>
                     <p>".$row['distance']." km</p>";
@@ -218,15 +199,24 @@ function notification($id_post) {
                     <p>".$row['lieu']."</p>
                     <p>".$count_like['nb_like']."</p>";
 
-                    $path = "../blog.php?blog=".$blog;
+                    // Mêmes divs que sur index.php (pas dans un include car le path n'est pas le même)
+                    echo "<div class='row'>
+                            <div class='column-third-no-bg'>
+                                <div class='box-invisible'>
+                                    <div class='boxtext'>
+                                        <p>".$count_like['nb_like']." encouragements au compteur !</p>
+                                    </div>
+                                    <div class='boximage'>";
+                                        $path = "../blog.php?blog=".$blog;
+                                        include("SousPages/like_comment.php");
+                                        //</div>s inclus dans ce include
 
-                    include("SousPages/like_comment.php");
-
+                    // Si on regarde des posts de notre propre blog, on a l'option de les supprimer ou de les modifier
                     if (isset($_COOKIE["id_utilisateur"])) {
                         if ($_COOKIE["id_utilisateur"] == $blog) {
                             
                             ?>
-                            <form action="SousPages/delete_post.php" onsubmit="return confirm('Etes vous sur de vouloir effacer?')" method='post'>
+                            <form action="SousPages/delete_post.php" onsubmit="return confirm('Etes-vous sur de vouloir effacer?')" method='post'>
                                 <input type='hidden' name='id_post' value='<?php echo $row['id_post'];?>'>
                                 <input type='submit' value='Supprimer'>
                             </form>
@@ -239,8 +229,7 @@ function notification($id_post) {
                         }
                     }
 
-                    echo "</div>
-                    <br><hr>
+                    echo "<br><hr>
                     <br>";
                 }
 
@@ -248,32 +237,26 @@ function notification($id_post) {
 
         ?>
 
-    </div>
+    </div> <!-- Fin de column-main -->
 
+    <!-- Onglet de recherche -->
     <div class="column-side">
 
         <?php
 
-            if(isset($_COOKIE["id_utilisateur"]) && $_COOKIE["id_utilisateur"] != $blog) {
+        if(isset($_COOKIE["id_utilisateur"]) && $_COOKIE["id_utilisateur"] != $blog) {
 
-                echo "
-                <form action='blog.php'>
-                        <input type='hidden' name='blog' value='".$_COOKIE['id_utilisateur']."'>
-                        <button type='submit'>Retourner sur mon blog</button>
-                </form>"; 
-                
-                
-
-
-            }
+            echo "
+            <form action='blog.php'>
+                    <input type='hidden' name='blog' value='".$_COOKIE['id_utilisateur']."'>
+                    <button type='submit'>Retourner sur mon blog</button>
+            </form>"; 
+        }
 
         ?>
 
-
         <div> 
-
-        <?php include("AJAX/recherche_blog.php"); ?> 
-
+            <?php include("AJAX/recherche_blog.php"); ?> 
         </div>
 
     </div>
